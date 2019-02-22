8075ad7aea9687ece978d52358135ecbf6d3fcc3
Chris <cwelde@uci.edu>
2019-02-21 16:20:49 -0800

    Added button and printing

--- app/src/main/java/com/example/weighthelper/MainActivity.java
+++ app/src/main/java/com/example/weighthelper/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.weighthelper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void test(View v)
+    {
+
+        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
     }
 }
